0dd9c9a29fd4193e5daf102676f2eba3e593a2c7
Ricky Holtz <eholtz@twilio.com>
2022-10-07 17:49:41 +0200

    Initial commit from plugin-signal2021 code

---
+++ src/commands/signal2021/index.js
@@ -0,0 +1,267 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const { baseCommands } = require('@twilio/cli-core');
+const { Flags } = require('@oclif/core');
+
+const { render } = require('../../components/App');
+const { stripIndent } = require('common-tags');
+const {
+  default: logger,
+  loggerPath,
+  cliLogLevelToPinoLogLevel,
+  pinoFinalHandler,
+  typeOut,
+  addNewLine,
+} = require('../../utils/logger');
+const { computeDiagnostics } = require('../../utils/diagnostics');
+const readline = require('readline');
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs');
+const { sleep } = require('../../utils/sleep');
+const { chalky } = require('../../utils/chalky');
+const { getRandomInt } = require('../../utils/maths');
+const { setTokenInPluginConfig } = require('../../utils/token');
+
+const { TwilioClientCommand } = baseCommands;
+
+let shouldCleanScreen = false;
+
+const baseFlags = { ...TwilioClientCommand.flags };
+baseFlags.profile.description =
+  "Shorthand identifier for your twilio-cli profile; run '$ twilio profiles:list' for more info.";
+
+class Signal2021Command extends TwilioClientCommand {
+  static strict = false;
+
+  static aliases = ['signal'];
+
+  static examples = ['$ twilio signal', '$ twilio signal end credits --tail'];
+
+  static flags = {
+    ...baseFlags,
+    diagnostics: Flags.boolean({
+      char: 'd',
+      default: false,
+      description:
+        'Using this flag will output diagnostics information that will be useful when debugging issues.',
+    }),
+    token: Flags.string({
+      description: 'The token used to give you access to SIGNAL.',
+    }),
+    feedback: Flags.boolean({
+      default: false,
+      description: 'Learn how you can give feedback on SIGNAL Developer Mode.',
+    }),
+    tail: Flags.boolean({
+      default: false,
+    }),
+  };
+
+  diagnostics = null;
+  token = null;
+
+  async catch(error) {
+    pinoFinalHandler(error, 'cliError', true);
+    if (error) {
+      let info;
+      if (this.logger & this.logger.info) {
+        info = this.logger.info;
+      } else {
+        info = console.log;
+      }
+      info(`Please check your log file for more details at: ${loggerPath}`);
+    }
+
+    return super.catch(error);
+  }
+
+  async checkForEndingCredits() {
+    const { argv, flags, logger } = this;
+
+    const end = argv[0] === 'end';
+    // Avoid edge case where user can fall into the normal flow by entering signal credits --tail
+    const credits = argv.includes('credits');
+
+    if (end || credits) {
+      if (end && credits && flags.tail) {
+        const creditsStream = readline.createInterface({
+          input: fs.createReadStream(
+            path.resolve(
+              __dirname,
+              '..',
+              '..',
+              '..',
+              'assets',
+              'end-credits.txt'
+            )
+          ),
+        });
+
+        let buffer = [];
+
+        const chalkyArtRegex = /^{([a-zA-Z]+)\s/;
+
+        for await (const line of creditsStream) {
+          if (line.startsWith('command')) {
+            await typeOut(line, 'command ');
+          } else if (line.startsWith('art')) {
+            buffer.push(line.replace('art ', ''));
+          } else if (line.startsWith('/art')) {
+            buffer.forEach((bufferedLine) => {
+              const customArtColors = chalkyArtRegex.test(bufferedLine);
+              console.log(
+                customArtColors
+                  ? chalky`${bufferedLine}` // this "works", but it gets really dicey and can crash the app depending on which characters are in the line 🙃
+                  : // @ts-ignore
+                    chalk.magentaBright(bufferedLine)
+              );
+            });
+            buffer = [];
+            await sleep(1500);
+          } else if (line.startsWith('dramaticPause')) {
+            const [_, duration] = line.split(' ');
+            await sleep(parseInt(duration) || 1000);
+          } else if (line.startsWith('yeetMeOuttaHere')) {
+            for (const command of ['^c', '^c']) {
+              // @ts-ignore
+              process.stdout.write(chalk.magentaBright(command));
+              await sleep(1000);
+            }
+            addNewLine();
+          } else if (line.startsWith('clear')) {
+            console.clear();
+          } else if (line.startsWith('vCenterText')) {
+            const { rows } = process.stdout;
+            const vPadding = Math.floor(rows / 2 - 1);
+            [...Array(vPadding)].forEach(addNewLine);
+            await typeOut(line, 'vCenterText ');
+          } else if (line.startsWith('totalCenter')) {
+            const newLine = line.replace('totalCenter ', '');
+            const { rows, columns } = process.stdout;
+            const vPadding = Math.floor(rows / 2 - 1);
+            [...Array(vPadding)].forEach(addNewLine);
+            // Seems sketchy, but works, at least for the logo
+            const hPadding = Math.floor((columns + newLine.length) / 2);
+            // @ts-ignore
+            console.log(chalk.yellowBright(newLine.padStart(hPadding)));
+          } else if (line.startsWith('hideCursor')) {
+            process.stdout.write('\x1B[?25l');
+          } else if (line.startsWith('showCursor')) {
+            process.stdout.write('\x1B[?25h');
+          } else {
+            await sleep(getRandomInt(350, 150));
+            console.log(chalky`${line}`);
+          }
+        }
+
+        return true;
+      }
+
+      logger.error(
+        'You must enter "twilio signal end credits --tail" exactly for this easter egg. Please try again!'
+      );
+      return true;
+    }
+
+    return false;
+  }
+
+  async checkFlags() {
+    if (Signal2021Command.flags.feedback) {
+      this.logger.info(
+        'We would love to hear what you think about SIGNAL Developer Mode. Head over to https://twil.io/signal-dev-mode-feedback'
+      );
+      return;
+    }
+
+    if (Signal2021Command.flags.tail) {
+      this.logger.info(
+        `Welcome! Using --tail doesn't actually do something but we are glad you are listening :)`
+      );
+    }
+
+    if (Signal2021Command.flags.diagnostics) {
+      this.output(this.diagnostics);
+      return;
+    }
+  }
+
+  checkForMintty() {
+    if (!process.stdout.isTTY) {
+      if (this.diagnostics.terminal === 'mintty') {
+        this.logger.info(stripIndent`
+          It appears as if you are using Git Bash or a similar to run SIGNAL Developer Mode.
+          Unfortunately Git Bash won't give you the best experience. Check out Windows Terminal as an alternative. If you still want to use it run next:
+          winpty twilio.cmd signal2021
+        `);
+        process.exit(1);
+      }
+
+      this.logger.info(
+        `We get it, you had to try it. We would have, too. However, you'll get the most out of SIGNAL Developer Mode by running it just using:\n$ twilio signal2021\nEnjoy SIGNAL!`
+      );
+      process.exit(0);
+    }
+  }
+
+  checkForGit() {
+    if (!this.diagnostics.hasGit) {
+      this.logger.info(
+        `We could not find git on your system. That means you won't be able to use the download & setup functionality of SIGNAL Developer Mode. You can still continue. If you wish to use that functionality, make sure you have git installed and it is part of your PATH. You might have to restart your terminal for this.`
+      );
+    }
+  }
+
+  async run() {
+    await super.run();
+    logger.level = cliLogLevelToPinoLogLevel(this.logger.config.level);
+    let debug =
+      this.logger.config.level === -1 || Boolean(process.env.DEBUG_SIGNAL);
+
+    this.diagnostics = computeDiagnostics(this);
+    this.logger.debug('Full log file at ' + loggerPath);
+    logger.info({
+      ...this.diagnostics,
+      msg: 'Diagnostics',
+      twilioCliLogLevel: this.logger.config.level,
+    });
+
+    const creditsAttempted = await this.checkForEndingCredits();
+    if (creditsAttempted) {
+      return;
+    }
+
+    await this.checkFlags();
+    await this.checkForMintty();
+    this.checkForGit();
+    shouldCleanScreen = !debug;
+    // TODO: Create some local state to capture the person's name?
+    await render(
+      {
+        name: 'Ricky',
+        accountSid: this.twilioClient.accountSid,
+        twilioUsername: this.twilioClient.username,
+        twilioPassword: this.twilioClient.password,
+      },
+      debug
+    );
+  }
+}
+
+module.exports = Signal2021Command;
+
+// catch all the ways node might exit
+process.on('beforeExit', () => pinoFinalHandler(null, 'beforeExit'));
+process.on('exit', () => {
+  if (shouldCleanScreen) {
+    const leaveAltScreenCommand = '\x1b[?1049l';
+    process.stdout.write(leaveAltScreenCommand);
+  }
+  return pinoFinalHandler(null, 'exit');
+});
+process.on('uncaughtException', (err) =>
+  pinoFinalHandler(err, 'uncaughtException')
+);
+process.on('SIGINT', () => pinoFinalHandler(null, 'SIGINT'));
+process.on('SIGQUIT', () => pinoFinalHandler(null, 'SIGQUIT'));
+process.on('SIGTERM', () => pinoFinalHandler(null, 'SIGTERM'));
---
+++ src/components/demo/DemoOptions.tsx
@@ -0,0 +1,64 @@
+import { Box } from 'ink';
+import React from 'react';
+import { DemoEntryOptions } from '../../machines/demoEntryMachine';
+import { Selectable } from '../common/Selectable';
+import { VerticalSpacer } from '../common/VerticalSpacer';
+
+export type DemoOptionsProps = {
+  hasQuickDeploy: boolean;
+  selectedOption: string;
+  hasDownloadOption: boolean;
+  hasViewCodeOption: boolean;
+  hasLearnMoreOption: boolean;
+};
+export function DemoOptions({
+  hasQuickDeploy,
+  selectedOption,
+  hasDownloadOption,
+  hasViewCodeOption,
+  hasLearnMoreOption,
+}: DemoOptionsProps) {
+  return (
+    <Box justifyContent="flex-start" marginTop={1}>
+      {hasLearnMoreOption && (
+        <Box>
+          <Selectable active={selectedOption === DemoEntryOptions.LEARN_MORE}>
+            Learn More
+          </Selectable>
+        </Box>
+      )}
+      {hasViewCodeOption && (
+        <>
+          <VerticalSpacer />
+          <Box>
+            <Selectable active={selectedOption === DemoEntryOptions.VIEW_CODE}>
+              View Code
+            </Selectable>
+          </Box>
+        </>
+      )}
+      {hasDownloadOption && (
+        <>
+          <VerticalSpacer />
+          <Box>
+            <Selectable active={selectedOption === DemoEntryOptions.DOWNLOAD}>
+              Download &amp; Setup
+            </Selectable>
+          </Box>
+        </>
+      )}
+      {hasQuickDeploy && (
+        <>
+          <VerticalSpacer />
+          <Box>
+            <Selectable
+              active={selectedOption === DemoEntryOptions.QUICK_DEPLOY}
+            >
+              Quick Deploy
+            </Selectable>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+}
