b8bd83bbee81f6292f1bce1d1dab0169a5408499
Shawn Stern <sstern@twilio.com>
2022-10-24 16:56:39 -0700

    chore: operators

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -16,9 +16,9 @@ export function sortByDate(a: Session, b: Session): number {
 export function timeToText(time: number): string {
   let rest = time;
   const hours = Math.floor(rest / ms('1h'));
-  rest = rest % ms('1h');
+  rest %= ms('1h');
   const minutes = Math.floor(rest / ms('1m'));
-  rest = rest % ms('1m');
+  rest %= ms('1m');
   const seconds = Math.floor(rest / 1000);
   return `${hours}:${minutes > 9 ? '' : '0'}${minutes}:${
     seconds > 9 ? '' : '0'
@@ -29,5 +29,6 @@ export function isDST(): boolean {
   const now = new Date();
   const jan = new Date(now.getFullYear(), 0, 1).getTimezoneOffset();
   const jul = new Date(now.getFullYear(), 6, 1).getTimezoneOffset();
+
   return Math.max(jan, jul) != now.getTimezoneOffset();
 }
